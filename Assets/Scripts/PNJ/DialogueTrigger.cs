21c185f6dfafcb3734ed05b3a26ca070f049dd0b
Renox91 <50325665+Renox91@users.noreply.github.com>
2021-11-13 12:02:03 +0100

    Ajout PNJ

---
+++ Assets/Scripts/PNJ/DialogueTrigger.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private Dialogue dialogue;
+    [SerializeField] private bool isInRange = false;
+    [SerializeField] private Quest quest;
+    void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E) && !quest.QuestIsAccepted)
+        {
+            TriggerDialogue();
+            quest.QuestIsAccepted = true;
+        }
+        else if(isInRange && Input.GetKeyDown(KeyCode.E) && quest.QuestIsAccepted)
+        {
+            if(quest.QuestIsFinished)
+            {
+                //Dialogue de fin de quête
+                // Ouverture de x objectifs
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                //Dialogue de quête
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            isInRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+            isInRange = false;
+    }
+
+    private void TriggerDialogue()
+    {
+        // Code pour trigger la boite de dialogue avec les paramètres
+    }
+}
---
+++ Assets/Scripts/PNJ/QuestTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && gameObject.GetComponentInParent<Quest>().QuestIsAccepted)
+        {
+            gameObject.GetComponentInParent<Quest>().QuestIsFinished = true;
+            gameObject.SetActive(false);
+        }
+    }
+}
